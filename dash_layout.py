250f72436dd0cf3dbee2b5259a4291af669bd796
sonasser <128932050+sonasser@users.noreply.github.com>
2023-04-21 18:22:33 +0100

    Add files via upload

---
+++ dash_layout.py
@@ -0,0 +1,49 @@
+# Import required packages
+
+import pandas as pd
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.express as px
+from dash.dependencies import Input, Output
+
+# Add Dataframe
+
+# Add a bar graph figure
+
+app = dash.Dash()
+app.layout = html.Div(children=[
+    html.H1(
+        children='Dashboard',
+        style={
+            'textAlign': 'center'
+        }
+    )
+    
+    # Create dropdown
+
+    # Bar graph
+]),
+
+   
+    # Create dropdown
+    dcc.Dropdown(options=[
+            {'label': 'New York City', 'value': 'NYC'},
+            {'label': 'Montréal', 'value': 'MTL'},
+            {'label': 'San Francisco', 'value': 'SF'}
+        ],
+        value='NYC' # Providing a vallue to dropdown
+    )
+
+# Run Application
+if __name__ == '__main__':
+    app.run_server()
+       
+    # Create dropdown
+    dcc.Dropdown(options=[
+            {'label': 'New York City', 'value': 'NYC'},
+            {'label': 'Montréal', 'value': 'MTL'},
+            {'label': 'San Francisco', 'value': 'SF'}
+        ],
+        value='NYC' # Providing a vallue to dropdown
+    )
